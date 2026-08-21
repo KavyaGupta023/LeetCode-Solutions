c10ab488621c734345019232c5532226d254d962
KavyaGupta023 <kavyagupta967@gmail.com>
2026-08-21 17:03:58 +0530

    Time: 1 ms (98.32%), Space: 42.5 MB (79.25%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low=1;
+        int high=x;
+        int ans=x;
+        while(low<=high){
+            int mid=low + (high - low) / 2;
+            if((long)mid*mid<=x){
+                ans=mid;
+                low=mid+1;
+            }
+            else
+            high=mid-1;
+        }
+        return (int)ans;
+    }
+}
