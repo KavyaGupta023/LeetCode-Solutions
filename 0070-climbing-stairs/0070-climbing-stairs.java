837e51432e7675375c108f8db39b2a2382165cbc
KavyaGupta023 <kavyagupta967@gmail.com>
2026-08-22 19:55:41 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.3%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,20 @@
 class Solution {
     int func(int n){
-        int dp[]=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
+        int pr1=1;
+        int pr2=2;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=pr2+pr1;
+            pr1=pr2;
+            pr2=curr;
         }
-        return dp[n];
+        return pr2;
 
     }
     public int climbStairs(int n) {
         if(n==0) return 0;
         if(n==1) return 1;
+        if(n==2) return 2;
+        
         return func(n);
     }
 }
