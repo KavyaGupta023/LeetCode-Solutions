9de3e8377dc17a0d855e3318117c2ac3660abeaa
KavyaGupta023 <kavyagupta967@gmail.com>
2026-08-24 11:00:04 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,7 +4,6 @@ class Solution {
         if(n==0) return 0;
         if(n==1) return nums[0];
         if(n==2) return Math.max(nums[0],nums[1]);
-       
         int dp[]=new int[n];
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
